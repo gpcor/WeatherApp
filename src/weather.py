95b43a5271f72f9d8afe2fcebd3a69545ccfd3ba
Gabe Corsini <gabe.corsini@gmail.com>
2020-09-29 19:16:17 -0400

    Updated weather app

--- src/weather.py
+++ src/weather.py
@@ -1,35 +1,31 @@
-import os
 import json
 import requests
 
 
-
 class WeatherApp:
-
-    def __init__(self):
-
-        os.chdir('/Users/gcorsini/Python/WeatherApp/src')
-
+    def __init__(self, lat, long, units):
+        self.lat = lat
+        self.long = long
+        self.units = units
         with open("api.txt", "r") as get_key:
             api_key = get_key.read()
-        lat = '36.8818'
-        lon = '-76.3989'
-        units = 'imperial'
-        timezone = 'EDT'
-        self.url = f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=minutely,hourly&units={units}&timezone={timezone}&appid={api_key}'
+        self.url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={long}&units={units}&appid={api_key}"
 
     def weather_lookup(self):
-        weather_json = requests.get(self.url.strip('\n')).json()
+        weather_json = requests.get(self.url.strip("\n")).json()
         with open('weather.json', 'a') as weather_to_file:
             json.dump(weather_json, weather_to_file, indent=4, sort_keys=True)
 
     def weather_format(self):
-        with open('weather.json', 'r+') as weather_from_file:
+        with open("weather.json", "r+") as weather_from_file:
             weather_from_file.truncate(0)
             weather_from_file.close()
 
 
-if __name__ == '__main__':
-    w = WeatherApp()
+if __name__ == "__main__":
+    lat = input("Enter lattitude: ")
+    long = input("Enter longitude: ")
+    units = input("Enter units (imperial or metric): ")
+
+    w = WeatherApp(lat, long, units)
     w.weather_lookup()
-    w.weather_format()
