9c152a13c4d592a76c27155884f856eda060d0f5
Gabe Corsini <gabe.corsini@gmail.com>
2020-10-19 23:25:42 -0400

    Pulled relevant weather data out into variables for email formatting. Removed redundant 'weather.json' file

--- src/weather.py
+++ src/weather.py
@@ -1,31 +1,68 @@
 import json
+import random
 import requests
 
 
 class WeatherApp:
-    def __init__(self, lat, long, units):
-        self.lat = lat
-        self.long = long
-        self.units = units
-        with open("api.txt", "r") as get_key:
-            api_key = get_key.read()
-        self.url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={long}&units={units}&appid={api_key}"
+    def __init__(self, city):
+        self.city = city
 
     def weather_lookup(self):
-        weather_json = requests.get(self.url.strip("\n")).json()
-        with open("weather.json", "a") as weather_to_file:
-            json.dump(weather_json, weather_to_file, indent=4, sort_keys=True)
+        with open("src/api.txt", "r") as get_key:
+            api_key = get_key.read()
+        self.url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={api_key}"
+        self.weather_json = requests.get(self.url.strip("\n")).json()
 
     def weather_format(self):
-        with open("weather.json", "r+") as weather_from_file:
-            weather_from_file.truncate(0)
-            weather_from_file.close()
+        self.location = self.weather_json["name"]
+        self.country = self.weather_json["sys"].get("country")
+        self.current_temp = round(self.weather_json["main"].get("temp"))
+        self.feels_like = round(self.weather_json["main"].get("feels_like"))
+        self.weather_description = self.weather_json["weather"][0]["description"]
+        self.wind_direction = self.weather_json["wind"].get("deg")
+        self.wind_speed = round(self.weather_json["wind"].get("speed"))
+
+        wind_direction_converstion = round((self.wind_direction / 22.5) + 0.5)
+        cardinal_directions = [
+            "N",
+            "NNE",
+            "NE",
+            "ENE",
+            "E",
+            "ESE",
+            "SE",
+            "SSE",
+            "S",
+            "SSW",
+            "SW",
+            "WSW",
+            "W",
+            "WNW",
+            "NW",
+            "NNW",
+        ]
+        self.wind_direction = cardinal_directions[(wind_direction_converstion % 16)]
+
+        print(
+            self.location,
+            self.country,
+            self.current_temp,
+            self.feels_like,
+            self.weather_description,
+            self.wind_direction,
+            self.wind_speed,
+        )
 
 
 if __name__ == "__main__":
-    lat = input("Enter lattitude: ")
-    long = input("Enter longitude: ")
-    units = input("Enter units (imperial or metric): ")
-
-    w = WeatherApp(lat, long, units)
+    city_names = []
+    with open("city.list.json", "r") as city_raw:
+        city_list = json.loads(city_raw.read())
+        for item in city_list:
+            for k, v in item.items():
+                if k == "name":
+                    city_names.append(v)
+    city = random.choice(city_names)
+    w = WeatherApp(city)
     w.weather_lookup()
+    w.weather_format()
